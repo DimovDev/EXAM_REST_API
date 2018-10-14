fdbe2fe31c869a89a0a0dbc797b482f1d337379c
Ivan Dimov <34308956+DimovDev@users.noreply.github.com>
2018-10-14 23:50:17 +0300

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+# from __future__ import unicode_literals
+
+from django.contrib import admin
+from .models import UserProfile, StatusUpdate, Message
+
+admin.site.register(UserProfile)
+admin.site.register(StatusUpdate)
+admin.site.register(Message)
